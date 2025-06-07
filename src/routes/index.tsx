ae15ca1ed522ebfb58dfe7957fd4868bb42783f8
akshay <psakshay17@gmail.com>
2025-06-07 18:12:15 +0530

    add years

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,62 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const [year, setYear] = useState<undefined | number>()
+
+  const yearHandler = (val: number) => {
+    setYear(val)
+  }
+
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    <section className="min-h-dvh flex flex-col items-center justify-center">
+      {year ? (
+        <h1>Select the month</h1>
+      ) : (
+        <>
+          <h1 className="text-2xl">Select the year</h1>
+          <div className="flex flex-col gap-8 items-center justify-center m-4 font-bold">
+            <div className="flex gap-8">
+              <Year year={2025} setYear={yearHandler} />
+              <Year year={2026} setYear={yearHandler} />
+              <Year year={2027} setYear={yearHandler} />
+              <Year year={2028} setYear={yearHandler} />
+            </div>
+            <div className="flex gap-8">
+              <Year year={2029} setYear={yearHandler} />
+              <Year year={2030} setYear={yearHandler} />
+              <Year year={2031} setYear={yearHandler} />
+            </div>
+            <div className="flex gap-8">
+              <Year year={2032} setYear={yearHandler} />
+              <Year year={2033} setYear={yearHandler} />
+              <Year year={2034} setYear={yearHandler} />
+              <Year year={2035} setYear={yearHandler} />
+            </div>
+          </div>
+        </>
+      )}
+    </section>
+  )
+}
+
+type Props = {
+  year: number
+  setYear: (val: number) => void
+}
+
+function Year({ year, setYear }: Props) {
+  return (
+    <button
+      type="button"
+      onClick={() => setYear(year)}
+      className="bg-white/30 py-4 px-8 text-2xl rounded-lg cursor-pointer"
+    >
+      {year}
+    </button>
   )
 }
